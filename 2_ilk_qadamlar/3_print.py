922cfe11e149faee6f4829ac42536c51faf345f0
Sardor Sultonnboyev <116072824+sard0r1@users.noreply.github.com>
2026-01-27 18:10:15 +0500

    Add/update 3_print.py

---
+++ 2_ilk_qadamlar/3_print.py
@@ -0,0 +1,21 @@
+# Amaliyot
+# # Quyidagi matnni aynan shunday ko'rinishda konsolda chiqaring:
+
+# # "Nexia", "Tico", 'Damas' ko'rganlar qilar havas
+
+# # Quyidagi misollarga yechimni Pythonda chiqaring. Har bir misoldan avval misol matnini izoh ko'rinishida yozing:
+
+# # 5 ning 4-darajasini toping
+# # 22 ni 4 ga bo'lganda qancha qoldiq qoladi?
+# # Tomonlari 125 ga teng kvadratning yuzi va perimetrini toping
+# # Diametri 12 ga teng bo'lgan doiraning yuzini toping (  π=3.14  deb oling)
+# # Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasini toping (Pifagor teoremasidan foydalaning)
+# # !! Javoblarni savol va yechim ko'rinishida chiqaring: 5 ning 4-darajasi 625 !!
+
+print("5 ning 4-darajasi", 5**4) # 5 ning 4-darajasi 625
+print("22 ni 4 ga bo'lganda qoldiq", 22%4) # 22 ni 4 ga bo'lganda qoldiq 2
+print("Tomonlari 125 ga teng kvadratning yuzi", 125*125) # Tomonlari 125 ga teng kvadratning yuzi 15625
+print("Tomonlari 125 ga teng kvadratning perimetri", 125*4) # Tomonlari 125 ga teng kvadratning perimetri 500
+print("Diametri 12 ga teng bo'lgan doiraning yuzi", 3.14*(12/2)**2) # Diametri 12 ga teng bo'lgan doiraning yuzi 113.04
+print("Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi", (6**2 + 7**2)**0.5) # Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi 9.219544457292887
+print("Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi", round((6**2 + 7**2)**0.5)) # Katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotenuzasi 9
