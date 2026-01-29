ab276fc1bfc5140a780b81871d0782242ce576bf
Sardor Sultonnboyev <116072824+sard0r1@users.noreply.github.com>
2026-01-29 12:38:48 +0500

    Add/update 1_ozgaruvchilar.py

---
+++ 3_ozgaruvchilar/1_ozgaruvchilar.py
@@ -0,0 +1,7 @@
+# "Hello World!" matnini yangi o'zgaruvchiga yuklang va print() yordamida konsolga chiqaring
+# xabar deb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring, keyin esa o'zgaruvchiga yangi qiymat berib uni ham konsolga chiqaring.
+xabar = "Hello World!"
+print(xabar)
+
+xabar = "Assalom alaykum"
+print(xabar)
