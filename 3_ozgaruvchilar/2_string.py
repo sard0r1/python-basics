198f6ea2748db70d0ec22850441919daf55e36c4
Sardor Sultonnboyev <116072824+sard0r1@users.noreply.github.com>
2026-01-31 12:50:17 +0500

    Add/update 2_string.py

---
+++ 3_ozgaruvchilar/2_string.py
@@ -0,0 +1,18 @@
+kocha = "Bog'bon" ; mahalla = "Sog'bon" ; tuman = "Bodomzor" ; viloyat = "Samarqand"
+# fstring = f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati"
+# print(fstring)
+
+# kocha = input("Ko'chani kiriting: ")
+# mahalla = input("Mahallani kiriting: ")
+# tuman = input("Tumanni kiriting: ")
+# viloyat = input("Viloyatni kiriting: ")
+# fstring = f"{kocha} ko'chasi, {mahalla} mahallasi, {tuman} tumani, {viloyat} viloyati"
+# print(fstring)
+
+# fstring = f"{kocha} ko'chasi,\n{mahalla} mahallasi,\n{tuman} tumani,\n{viloyat} viloyati"
+# print(fstring)
+
+manzil = f"{kocha.title()} ko'chasi, {mahalla.upper()} mahallasi, {tuman.lower()} tumani, {viloyat.capitalize()} viloyati"
+print(manzil)
+
+son = input("Istalgan sonni kiriting: ")
