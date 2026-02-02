4ddb2977e020a3169f0649880da065d7a32fa66f
Sardor Sultonnboyev <116072824+sard0r1@users.noreply.github.com>
2026-02-02 10:21:36 +0500

    Add/update 3_sonlar.py

---
+++ 3_ozgaruvchilar/3_sonlar.py
@@ -0,0 +1,13 @@
+# son = int(input("Istalgan sonni kiriting: "))
+# print(f"{son} ning kvadrati {son**2} ga teng")
+# print(f"{son} ning kubi {son**3} ga teng")
+
+# yosh = int(input("Yoshingizni nechida: "))
+# print(f"Siz {2026-yosh} yilda tug'ilgansiz")
+
+a = int(input("Birinchi sonni kiriting: "))
+b = int(input("Ikkinchi sonni kiriting: "))
+print(f"{a} + {b} =" , a+b )
+print(f"{a} - {b} = {a-b}")
+print(f"{a} * {b} = {a*b}")
+print(f"{a} / {b} = {a/b}")
