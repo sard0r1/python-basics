c031ee8be1bc7f43427e69f10e4305cab62d6bc4
Sardor Sultonnboyev <116072824+sard0r1@users.noreply.github.com>
2026-02-06 15:42:43 +0500

    Add/update 5_list2.py

---
+++ 3_ozgaruvchilar/5_list2.py
@@ -0,0 +1,32 @@
+# davlatlar = ["uzb" , "rus" , "kaz" ]
+
+# # print(len(davlatlar))
+# # print(sorted(davlatlar))
+# # print(sorted(davlatlar))
+# # print(davlatlar)
+# davlatlar.reverse()
+# print(davlatlar)
+# davlatlar.sort()
+# print(davlatlar)
+# davlatlar.sort(reverse=True)
+# print(davlatlar)
+
+# sonlar = list(range(120,1200,2))
+# # print(sonlar)
+# print(sum(sonlar))
+# print(max(sonlar))
+# print(min(sonlar))
+# print(sonlar[0:20])
+# print(sonlar[-20:])
+# mid = len(sonlar)//2
+# print(sonlar[mid-10:mid+10])
+
+taomlar = ["osh" , "shorva" , 'qovurdaq' , 'barak' , 'manti']
+nonushta = taomlar[:]
+nonushta.append('tuxum')
+print(taomlar)
+print(nonushta)
+
+nonushta = tuple(nonushta)
+print(nonushta)
+# nonushta[0] = 'qaymoq'
