cd05e352a1c4e86735fc2048036be8252d1c0bfa
Sardor Sultonnboyev <116072824+sard0r1@users.noreply.github.com>
2026-02-10 19:03:23 +0500

    Add/update 1_if.py

---
+++ 4_shartlar/1_if.py
@@ -0,0 +1,39 @@
+# # # # cars = ["toyota" , "mazda" , 'hyundai' , 'gm' , 'kia']
+# # # # for car in cars:
+# # # #     if car == "gm":
+# # # #         print("GM")
+# # # #     else :
+# # # #         print(car.title())
+
+# # # cars = ["toyota" , "mazda" , 'hyundai' , 'gm' , 'kia']
+# # # for car in cars:
+# # #     if car != "gm":
+# # #         print(car.title())
+# # #     else :
+# # #         print(car.upper())
+
+
+# # login = input('login ismingiz :')
+# # if login == "admin":
+# #     print('hush kelibsiz admin, foydalanuvchilar royxatini ko\'rasizmi')
+# # else:
+# #     print('hush kelibsiz ' , login)
+
+# a = input('son kiriting: ')
+# b = input('ikkinchi sonni kiriting: ')
+# if a == b:
+#     print('sonlar teng ekan')
+
+# a = int(input('istalgan son kiriting: '))
+# if a>=0:
+#     print('musbat')
+# else:
+#     print('manfiy')
+
+# a = int(input('istalgan son kiriting: '))
+# if a>=0:
+#     print(a**(1/2))
+# else:
+#     print('musbat son kiriting')
+
+
